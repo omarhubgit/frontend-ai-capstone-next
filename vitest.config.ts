25ce10f772eeca9cfc4f45d989fbb5f60288e520
Omar Abdullah <omarabdullahmahmoud@gmail.com>
2026-08-30 04:25:31 +0300

    test: configure vitest test files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./vitest.setup.ts",
+    include: ["tests/**/*.{test,spec}.{ts,tsx}"],
   },
 });
